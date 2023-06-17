ce25879a4400069db4d401c5cd58b9f7d10d7e57
Rabin <rabin.shrestha7@gmail.com>
2023-06-17 08:35:11 -0700

    practise consumer 1

---
+++ src/main/java/com/kafkaConsumer1/practiseConsumer1/PractiseConsumer1Application.java
@@ -0,0 +1,13 @@
+package com.kafkaConsumer1.practiseConsumer1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PractiseConsumer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PractiseConsumer1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafkaConsumer1/practiseConsumer1/PractiseConsumer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkaConsumer1.practiseConsumer1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PractiseConsumer1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
